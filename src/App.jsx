d4446f149ab677f77901dd2ba81bb4c0f3894069
Ijechidi <chidinma.ijeom@gmail.com>
2024-09-27 16:50:47 +0000

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import SignInForm from "./pages/Sign-in/SignIn";
+import SignUpForm from "./pages/Sign-up/SignUp";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
+        <Route path="/Sign-up/" element={<SignUpForm />}></Route>
+        <Route path="/Sign-in/" element={<SignInForm />}></Route>
+     
       </Routes>
     </BrowserRouter>
   );
