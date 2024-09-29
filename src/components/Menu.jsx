843c42746f7c7cb272ef1735637d48a5f8695de1
OmowunmiKamil <balikiskamil@gmail.com>
2024-09-29 08:20:42 +0100

    Added admin Menu form

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,7 +5,7 @@ function Menu({ items }) {
     <div className="border-2 border-p-button px-8 pt-8">
       <img src={items.src} alt="image" />
       <p className="my-4">{items.text}</p>
-      <div className="inline-block"></div>
+      <div className="inline-block">{items.text}</div>
     </div>
   );
 }
