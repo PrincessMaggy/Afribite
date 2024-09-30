0caeac1771404b59d1ce5c6a6557c29cb3c97ac1
Mag <166319982+magali-la@users.noreply.github.com>
2024-09-30 02:47:13 +0200

    finished notification popup styling

--- src/components/OrderPopup.jsx
+++ src/components/OrderPopup.jsx
@@ -1,17 +1,17 @@
 
 const OrderPopup = () => {
     return (
-        <div className="flex-col justify-center items-center gap-y-6 bg-notif w-[281px] h-[387px] border-solid border-t-2 rounded-[10px] font-normal text-[#333333] leading-5">
-            <div className="text-[#333333] w-[232px] leading-8 text-sm">
-                <p className="text-">Hi UserName,</p>
-                <p>You've received a new order from CustomerName!</p>
-                <p>Order Details:</p>
+        <div className="flex flex-col justify-center items-center bg-notif w-[281px] text-[#333333] font-light gap-y-5 py-[21px] px-[23px] rounded-lg leading-5">
+            <div className="text-sm">
+                <p>Hi UserName,</p><br/>
+                <p>You've received a new order from CustomerName!</p><br/>
+                <p className="font-normal">Order Details:</p>
                 <p>Quantity:</p>
-                <p>Total:</p>
+                <p>Total:</p><br/>
                 <p>Please Start Preparing the Order.</p><br/>
                 <p>You can view More Details or Update the Order Status.</p>
             </div>
-            <button className="bg-[#e2725b] w-[127px] h-[10px] text-notif text-lg border rounded-lg">Update Status</button>
+            <button className="bg-accent text-notif text-lg p-4 rounded-[10px]">Update Status</button>
         </div>
 
     ); 
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,11 @@ export default {
         'notif':'#fbf5e9',
         'notif-bord':'#f4e1c133',
         'accent': '#e2725b',
-        'main-text':'#333333',
+      },
+      spacing: {
+        '10px': '10px',
+        '21px': '21px',
+        '23px': '23px',
       },
     },
   },
