cc61c850a1166e5f368a6248e4162ac33a6f5fcf
Precious <chidinma.ijeom@gmail.com>
2024-10-04 17:28:42 +0000

    Update button.jsx

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,13 +1,16 @@
 import react from "react";
+import { Link } from "react-router-dom";
 
 const Button = (props) => {
   return (
-    <button
-      className={`bg-p-button p-3 rounded-md text-n-n7 text-xs lg:text-sm font-pop border-2 hover:border-p-button hover:text-p-button hover:bg-n-n7 ${props.className}`}
-      onClick={props.onClick}
-    >
-      {props.text}
-    </button>
+    <Link to={props.to}>
+      <button
+        className={bg-p-button p-3 rounded-md text-n-n7 text-xs lg:text-sm font-pop border-2 hover:border-p-button hover:text-p-button hover:bg-n-n7 ${props.className}}
+        onClick={props.onClick}
+      >
+        {props.text}
+      </button>
+    </Link>
   );
 };
 
