6836a61eb43a9397a66e899a4e8a295080745beb
OmowunmiKamil <balikiskamil@gmail.com>
2024-10-06 06:19:56 +0100

    Updated Readme file

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,13 @@ import Notifications from "./pages/Notifications";
 import Promotions from "./pages/Promotion";
 
 import MainDish from "./pages/MainDish";
+import Appetizer from "./pages/Appetizer";
+import Side from "./pages/SIde";
+import Soup from "./pages/Soup";
+import Salad from "./pages/Salad";
+import Special from "./pages/Special";
+import Dessert from "./pages/Dessert";
+import Beverage from "./pages/Beverage";
 
 function App() {
   return (
@@ -33,6 +40,13 @@ function App() {
                   element={<Notifications />}
                 ></Route>
                 <Route path="/MainDish" element={<MainDish />}></Route>
+                <Route path="/Appetizer" element={<Appetizer />}></Route>
+                <Route path="/Side" element={<Side />}></Route>
+                <Route path="/Soup" element={<Soup />}></Route>
+                <Route path="/Salad" element={<Salad />}></Route>
+                <Route path="/Special" element={<Special />}></Route>
+                <Route path="/Beverage" element={<Beverage />}></Route>
+                <Route path="/Dessert" element={<Dessert />}></Route>
               </Routes>
             </Layout>
           }
