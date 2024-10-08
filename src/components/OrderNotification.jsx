efdc4494f18dca169fac40dbf4238f486c21fa88
Mag <166319982+magali-la@users.noreply.github.com>
2024-10-08 19:17:34 +0200

    combined duplicate color section in tailwind, fixed syntax

--- src/components/OrderNotification.jsx
+++ src/components/OrderNotification.jsx
@@ -1,24 +1,16 @@
 import ActiveIcon from '../assets/active.svg'
-import Button from './button'
+import Button from './button.jsx'
 
 const OrderNotification = ({orderTime, customerName, orderName, orderQuantity, orderPrice}) => {
-    return(
-        <div className="flex flex-row justify-between items-center w-[736px] p-y-[9px] p-x-[23px] bg-notif">
-            <img src={ActiveIcon} className='size-2.5'/>
+    return (
+        <div className="flex flex-row justify-between items-center w-[736px] bg-[eggshell]">
+            <img src={ActiveIcon} className="w-10 h-10"/>
             <p>{orderTime}</p>
             <p>{customerName} has placed an order for {orderName}</p>
             <p>£{orderPrice}</p>
-            <Button
-                text="View Order"
-                className="rounded-sm h-8 bg-[#808000] hover:border-[#808000] hover:text-[#808000] hover:bg-n-n7 "
-
-            />
-
-            
+            <Button/>
         </div>
-
-    
     );
-};
+}
 
-export default OrderNotification;
+export default OrderNotification
