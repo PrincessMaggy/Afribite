48a801cfec00f2e47ce203e659aaae3fc66a4dd5
Edozie Magdalene <edoziemagdalene@gmail.com>
2024-10-09 09:04:08 +0100

    Revert "removed conflicting tailwind css custom colors"

--- src/components/OrderNotification.jsx
+++ src/components/OrderNotification.jsx
@@ -1,16 +1,24 @@
 import ActiveIcon from '../assets/active.svg'
-import Button from './button.jsx'
+import Button from './button'
 
 const OrderNotification = ({orderTime, customerName, orderName, orderQuantity, orderPrice}) => {
-    return (
-        <div className="flex flex-row justify-between items-center w-[736px] bg-[eggshell]">
-            <img src={ActiveIcon} className="w-10 h-10"/>
+    return(
+        <div className="flex flex-row justify-between items-center w-[736px] p-y-[9px] p-x-[23px] bg-notif">
+            <img src={ActiveIcon} className='size-2.5'/>
             <p>{orderTime}</p>
             <p>{customerName} has placed an order for {orderName}</p>
             <p>£{orderPrice}</p>
-            <Button/>
-        </div>
-    );
-}
+            <Button
+                text="View Order"
+                className="rounded-sm h-8 bg-[#808000] hover:border-[#808000] hover:text-[#808000] hover:bg-n-n7 "
 
-export default OrderNotification
+            />
+
+            
+        </div>
+
+    
+    );
+};
+
+export default OrderNotification;
