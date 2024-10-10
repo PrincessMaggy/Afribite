26e887b46d140f5e6287dbd423d76fe7c7a361fa
Mag <166319982+magali-la@users.noreply.github.com>
2024-10-10 15:06:28 +0200

    merge upstream changes

--- src/components/OrderSearch.jsx
+++ src/components/OrderSearch.jsx
@@ -2,7 +2,7 @@ import { CiSearch } from "react-icons/ci";
 
 const OrderSearch = () => {
     return(
-        <div className="flex flex-row items-center ml-10 w-3/4 bg-inherit border border-[#E2725B]/20 focus:outline-none focus:border-[#E2725B] rounded-3xl">
+        <div className="flex flex-row items-center ml-10 bg-inherit border border-[#E2725B]/20 focus:outline-none focus:border-[#E2725B] rounded-3xl">
           <CiSearch className='text-2xl text-n-n3'/>
           <input type="text" className="w-3/4 px-12 bg-inherit border-none" placeholder='Seach for Orders'/>
             
