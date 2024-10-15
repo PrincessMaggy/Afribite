b7c6c47ad0cd0ea10781601d33535d8358a3a7d0
Mag <166319982+magali-la@users.noreply.github.com>
2024-10-16 01:01:55 +0200

    tabs integration for order sorting

--- src/components/OrderTabs.jsx
+++ src/components/OrderTabs.jsx
@@ -2,29 +2,64 @@ import React, {useState} from 'react';
 import OrderNotification from './OrderNotification';
 
 const OrderTabs = () => {
-    const [activeTab, setActiveTab] = useState(0)
+    const [activeTab, setActiveTab] = useState(0);
 
     const tabs = [
         {
             title: 'New',
-            content:[],
+            content:[
+                <OrderNotification
+                    id="1"
+                    orderStatus="New"
+                    orderTime="10mins" 
+                    customerName="Favour Chinedu"
+                    orderName="Jollof Rice"
+                    orderQuantity="2"
+                    orderPrice="20"
+                />,
+            ],
         },
         {
             title: 'Finished',
-            content:[],
+            content:[
+                <OrderNotification
+                    id="2"
+                    orderStatus="Ready for Delivery"
+                    orderTime="30mins"
+                    customerName="Ade Oluwa"
+                    orderName="Suya"
+                    orderQuantity="1"
+                    orderPrice="15"
+                />,
+            ],
         },
         {
             title: 'Delivered',
-            content:[],
+            content:[
+                <OrderNotification
+                    id="3"
+                    orderStatus="Delivered"
+                    orderTime="45mins"
+                    customerName="Amaka Nwoke"
+                    orderName="Egusi Soup"
+                    orderQuantity="3"
+                    orderPrice="25"
+                />,
+            ],
         },
-    ]
+    ];
 
     return (
         <div className='flex flex-row items-center gap-12 h-8 border w-full rounded-lg'>
-            <button className='border-b-4 border-p-button'>New</button>
-            <button>Finished</button>
-            <button>Delivered</button>
-
+            {tabs.map((tab,index) => (
+                <button
+                key={index}
+                className={`${activeTab === index ? 'border-b-2 border-p-button text-p-button' : ''}`}
+                onClick={() => setActiveTab(index)}
+                >
+                {tab.title}
+                </button>
+            ))}
         </div>
     );
 };
