1bfbff0ad3a63ed6a2acd251de3cebbf8c3046be
Mag <166319982+magali-la@users.noreply.github.com>
2024-10-19 14:47:08 +0200

    data sorting, grid layouts, style for notifications comp

--- src/components/OrderPopup.jsx
+++ src/components/OrderPopup.jsx
@@ -1,34 +1,34 @@
 import React, {useState} from 'react';
 import Button from "./button";
 
-const OrderPopup = ({adminName, customerName, orderName, orderQuantity, orderPrice, popupOpen, toggleOrderPopup, orderStatus, updateOrderStatus, deliveryTime }) => {
+const OrderPopup = ({customerName, orderName, orderNumber, orderQuantity, orderPrice, popupOpen, toggleOrderPopup, orderStatus, updateOrderStatus, deliveryTime }) => {
+    
     return (
         popupOpen && (
         <div className="flex flex-col justify-center items-center bg-notif border-2 border-terra-cotta w-[281px] text-n-n1 font-light gap-y-5 py-[21px] px-[23px] rounded-lg leading-5">
             <div className="text-sm text-n-n1">
-                {orderStatus === "New" && (
+                {orderStatus === "new" && (
                     <div>
-                    <h2 className='text-xl font-normal text-p-button'>Order Details</h2>
-                    <p>Hi {adminName},</p><br/>
-                    <p>You've received a new order from {customerName}!</p><br/>
-                    <p className="font-normal">Order Details:</p>
+                    <h2 className='text-xl font-normal text-p-button'>Order Details: #{orderNumber}</h2>
+                    <p>New order {orderNumber} from {customerName}!</p><br/>
                     <p>Dish: {orderName}</p>
                     <p>Quantity: {orderQuantity}x</p>
                     <p>Total: £{orderPrice}</p><br/>
                     <p>Please Start Preparing the Order.</p><br/>
                     <p>You can view More Details or Update the Order Status.</p>
-                    <Button 
-                        text="Finished Cooking"
-                        className="bg-accent text-notif text-lg p-4 rounded-lg"
-                        onClick={() => updateOrderStatus("Finished")}
-                    />
+                    <div>
+                        <Button 
+                            text="Finished Cooking"
+                            className="bg-accent text-notif text-lg rounded-lg"
+                            onClick={() => updateOrderStatus("ongoing")}
+                        />
+                    </div>
                     </div>
                 )}
                 
 
-                {orderStatus === "Finished" && (
+                {orderStatus === "ongoing" && (
                     <div>
-                    <p>Hi {adminName},</p><br/>
                     <p>Order #324 for {customerName} is finished and ready for delivery or pickup.</p><br/>
                     <p className="font-normal">Order Details:</p>
                     <p>Dish: {orderName}</p>
@@ -36,16 +36,15 @@ const OrderPopup = ({adminName, customerName, orderName, orderQuantity, orderPri
                     <p>Total: £{orderPrice}</p><br/>
                     <p>Take the next step to complete the order.</p><br/>
                     <Button
-                      text="Ready for Delivery"
+                      text="Ready for Pickup"
                       className="bg-accent text-notif text-lg p-4 rounded-lg"
-                      onClick={() => updateOrderStatus("Delivered")}
+                      onClick={() => updateOrderStatus("pickup")}
 
                     /> 
                     </div>             
                 )}
-                {orderStatus === "Delivered" && (
+                {orderStatus === "delivered" && (
                     <div>
-                    <p>Hi {adminName},</p><br/>
                     <p>The delivery for Order #324 has been completed! {customerName} has received their meal, and your service was outstanding.</p><br />
                     <p className="font-normal">Order Details:</p>
                     <p>Dish: {orderName}</p>
