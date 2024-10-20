1cf7acbc91085d8c873e11c632c60c92d179bff8
OmowunmiKamil <balikiskamil@gmail.com>
2024-10-20 03:42:08 +0100

    Added menu context to dashboard to generate admin total available dish

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,176 +1,199 @@
-import bannerImage from '../assets/image.png'
-import dishIcon from '../assets/dishicon.png'
-import vector from '../assets/Vector.png'
+import bannerImage from "../assets/image.png";
+import dishIcon from "../assets/dishicon.png";
+import vector from "../assets/Vector.png";
 import { IoIosArrowDropdown } from "react-icons/io";
-import RecentOrder from '../components/RecentOrder';
-import { dummyOrders,dishes } from '../components/data';
-import { useContext, useEffect, useState } from 'react';
-import ViewersChart from '../components/ViewersChart';
-import ProfileDisplay from '../components/ProfileDisplay';
-import { profileContext } from '../context/ProfileContext';
-import PointChart from '../components/PointChart';
-import CustomerChart from '../components/CustomerChart';
-import ProgressBar from '../components/ProgressBar';
+import RecentOrder from "../components/RecentOrder";
+import { dummyOrders, dishes } from "../components/data";
+import { useContext, useEffect, useState } from "react";
+import ViewersChart from "../components/ViewersChart";
+import ProfileDisplay from "../components/ProfileDisplay";
+import { profileContext } from "../context/ProfileContext";
+import PointChart from "../components/PointChart";
+import CustomerChart from "../components/CustomerChart";
+import ProgressBar from "../components/ProgressBar";
+import { menuContext } from "../context/MenuContext";
 
 const Dashboard = () => {
-  const {myProfile, userAdData, promotions, setPromotions} = useContext(profileContext)
-  const [myOrders, setMyOrders] = useState([])
+  const { myProfile, userAdData, promotions, setPromotions } =
+    useContext(profileContext);
+  const [myOrders, setMyOrders] = useState([]);
 
+  const { menuLength, setMenuData } = useContext(menuContext);
 
+  const totalOrders = myOrders.length;
 
-  const totalOrders = myOrders.length
+  const recentOrders = myOrders.slice(0, 3);
 
-  const recentOrders = myOrders.slice(0,3)
+  const totalDishes = dishes.length;
 
-  const totalDishes = dishes.length
-
-  useEffect(()=>{
-    setMyOrders(dummyOrders
-      .filter((items)=>items.userid === 'user1'))
-  },[])
-
+  useEffect(() => {
+    setMyOrders(dummyOrders.filter((items) => items.userid === "user1"));
+  }, []);
 
   return (
     <div className="">
       <div className="grid grid-cols-3">
-
         {/* left side */}
-      <div className="lg:col-span-2 col-span-3 ">
-
-        {/* top banner */}
-        <div className='w-full bg-white rounded-lg overflow-hidden'>
-          <div className="grid grid-cols-2">
-              <div className='flex flex-col m-auto gap-1 lg:gap-5 p-4 '>
-                  <h1 className='text-black text-xl md:text-2xl text-center'>
-                      Hi, {myProfile?.restuarantName}
-                  </h1>
-                  <p className='text-sm md:text-base lg:text-lg text-black text-center'>Welcome to <span className='text-[#E2725B]'>AfriBite</span></p>
-
+        <div className="lg:col-span-2 col-span-3 ">
+          {/* top banner */}
+          <div className="w-full bg-white rounded-lg overflow-hidden">
+            <div className="grid grid-cols-2">
+              <div className="flex flex-col m-auto gap-1 lg:gap-5 p-4 ">
+                <h1 className="text-black text-xl md:text-2xl text-center">
+                  Hi, {myProfile?.restuarantName}
+                </h1>
+                <p className="text-sm md:text-base lg:text-lg text-black text-center">
+                  Welcome to <span className="text-[#E2725B]">AfriBite</span>
+                </p>
               </div>
-              <div className=' lg:h-56  flex'><img src={bannerImage} alt="" className="flex-1 lg:w-full" /></div>
-            </div>
-        </div>
-
-        {/* Available dish and total orders */}
-        <div className='grid grid-cols-2 gap-4 mt-4 '>
-
-          <div className='relative bg-white rounded-lg p-4 '>
-            <span className="absolute right-5 top-6 md:right-10 md:top-5 md:text-3xl text-n-n3"><IoIosArrowDropdown /></span>
-
-            <p className="text-center my-2">Available Dish</p>
-
-            <div className='w-full m-auto flex items-center px-2'>
-              <img src={dishIcon} alt="" className="rounded-full bg-[#E2725B]/50 p-1 border-2 border-red-300 ml-5 h-10 md:h-full" />
-              <div className='text-black/60 md:text-4xl font-semibold text-center flex-1 md:mr-14'>
-                {totalDishes}
-              </div>
-            </div>
-
-            <div className="w-full flex my-2 md:my-4">
-              <div className='px-2 py-1 rounded-2xl border-[#808000] bg-[#808000]/30 m-auto'>
-              <p className="">+ 10 New Add</p>
-              </div>
-            </div>
-
-          </div>
-
-          <div className='relative bg-white rounded-lg p-4'>
-
-            <span className="absolute right-5 top-6 md:right-10 md:top-5 md:text-3xl text-n-n3"><IoIosArrowDropdown /></span>
-
-            <p className="text-center my-2">Total Orders</p>
-
-            <div className='w-full m-auto flex items-center px-2'>
-              <img src={vector} alt="" className="rounded-full bg-yellow-200/50 p-1 border-2 border-yellow-200 h-10 md:h-full" />
-              <div className='text-black/60 md:text-4xl font-semibold text-center flex-1 md:mr-12 '>
-                    {totalOrders}
-              </div>
-            </div>
-
-
-            <div className="w-full flex my-2 md:my-4">
-              <div className='px-2 py-1 rounded-2xl border-[#808000] bg-[#808000]/30 m-auto'>
-              <p className="">+ 10 New Add</p>
+              <div className=" lg:h-56  flex">
+                <img src={bannerImage} alt="" className="flex-1 lg:w-full" />
               </div>
             </div>
           </div>
 
-        </div>
+          {/* Available dish and total orders */}
+          <div className="grid grid-cols-2 gap-4 mt-4 ">
+            <div className="relative bg-white rounded-lg p-4 ">
+              <span className="absolute right-5 top-6 md:right-10 md:top-5 md:text-3xl text-n-n3">
+                <IoIosArrowDropdown />
+              </span>
 
+              <p className="text-center my-2">Available Dish</p>
 
-        <div className='grid md:grid-cols-2 gap-4 mt-4 '>
-
-          <div className="bg-white rounded-lg p-8 flex flex-col gap-5 md:gap-0 justify-around">
-            <h2 className='font-medium text-lg'>Total Summary</h2>
-
-            <div className="">
-              <div className="flex justify-between">
-                <p>Promotions</p>
-                <p>{promotions.length}/100</p>
+              <div className="w-full m-auto flex items-center px-2">
+                <img
+                  src={dishIcon}
+                  alt=""
+                  className="rounded-full bg-[#E2725B]/50 p-1 border-2 border-red-300 ml-5 h-10 md:h-full"
+                />
+                <div className="text-black/60 md:text-4xl font-semibold text-center flex-1 md:mr-14">
+                  {menuLength}
+                </div>
               </div>
-              <ProgressBar value={promotions.length} color={'p-button'} max='100'/> 
-            </div>
 
-            <div className="">
-              <div className="flex justify-between">
-                <p>Advertisment</p>
-                <p>{userAdData.length}/100</p>
+              <div className="w-full flex my-2 md:my-4">
+                <div className="px-2 py-1 rounded-2xl border-[#808000] bg-[#808000]/30 m-auto">
+                  <p className="">+ 10 New Add</p>
+                </div>
               </div>
-              <ProgressBar value={userAdData.length} color={'p-button3'} max='100'/> 
+            </div>
+
+            <div className="relative bg-white rounded-lg p-4">
+              <span className="absolute right-5 top-6 md:right-10 md:top-5 md:text-3xl text-n-n3">
+                <IoIosArrowDropdown />
+              </span>
+
+              <p className="text-center my-2">Total Orders</p>
+
+              <div className="w-full m-auto flex items-center px-2">
+                <img
+                  src={vector}
+                  alt=""
+                  className="rounded-full bg-yellow-200/50 p-1 border-2 border-yellow-200 h-10 md:h-full"
+                />
+                <div className="text-black/60 md:text-4xl font-semibold text-center flex-1 md:mr-12 ">
+                  {totalOrders}
+                </div>
+              </div>
+
+              <div className="w-full flex my-2 md:my-4">
+                <div className="px-2 py-1 rounded-2xl border-[#808000] bg-[#808000]/30 m-auto">
+                  <p className="">+ 10 New Add</p>
+                </div>
+              </div>
             </div>
           </div>
 
-          <div className="bg-white rounded-lg p-4  flex flex-col">
-            <h2 className="font-medium text-lg">Performance</h2>
+          <div className="grid md:grid-cols-2 gap-4 mt-4 ">
+            <div className="bg-white rounded-lg p-8 flex flex-col gap-5 md:gap-0 justify-around">
+              <h2 className="font-medium text-lg">Total Summary</h2>
 
-            <div className="h-36"><PointChart/></div>
+              <div className="">
+                <div className="flex justify-between">
+                  <p>Promotions</p>
+                  <p>{promotions.length}/100</p>
+                </div>
+                <ProgressBar
+                  value={promotions.length}
+                  color={"p-button"}
+                  max="100"
+                />
+              </div>
 
-            <p className='text-center'>Earn more points and boost your restaurant’s visibility</p>
-          </div>
+              <div className="">
+                <div className="flex justify-between">
+                  <p>Advertisment</p>
+                  <p>{userAdData.length}/100</p>
+                </div>
+                <ProgressBar
+                  value={userAdData.length}
+                  color={"p-button3"}
+                  max="100"
+                />
+              </div>
+            </div>
 
-        </div>
+            <div className="bg-white rounded-lg p-4  flex flex-col">
+              <h2 className="font-medium text-lg">Performance</h2>
 
-        <div className='grid md:grid-cols-2 gap-4 mt-4 '>
+              <div className="h-36">
+                <PointChart />
+              </div>
 
-          <div className="bg-white rounded-lg p-4">
-          <h2 className="font-medium text-lg">Views</h2>
-            <div className='h-52'> <ViewersChart/></div>
-          </div>
-
-          <div className="bg-white rounded-lg p-4">
-            <div className="w-full">
-            <h2 className="font-medium text-lg">Your Customers</h2>
-            <div className="h-52"><CustomerChart/></div>
+              <p className="text-center">
+                Earn more points and boost your restaurant’s visibility
+              </p>
             </div>
           </div>
 
+          <div className="grid md:grid-cols-2 gap-4 mt-4 ">
+            <div className="bg-white rounded-lg p-4">
+              <h2 className="font-medium text-lg">Views</h2>
+              <div className="h-52">
+                {" "}
+                <ViewersChart />
+              </div>
+            </div>
+
+            <div className="bg-white rounded-lg p-4">
+              <div className="w-full">
+                <h2 className="font-medium text-lg">Your Customers</h2>
+                <div className="h-52">
+                  <CustomerChart />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
 
+        {/* right side */}
+        <div className=" ml-6 hidden lg:flex flex-col justify-between">
+          {/* profile info display */}
+          <div className="bg-white px-8 p-4">
+            <ProfileDisplay />
+          </div>
 
-      </div>
-
-      {/* right side */}
-      <div className=" ml-6 hidden lg:flex flex-col justify-between">
-
-        {/* profile info display */}
-        <div className="bg-white px-8 p-4">
-          <ProfileDisplay/>
+          {/* recent orders */}
+          <div className="bg-white px-2 p-4 mt-4 flex flex-col gap-2">
+            <h1 className="text-center text-l font-medium my-4 ">
+              Recent Order
+            </h1>
+            {recentOrders.map((items, index) => (
+              <RecentOrder
+                key={index}
+                image={items.dishImg}
+                dish={items.mealname}
+                price={items.price}
+                date={items.date}
+                status={items.status}
+              />
+            ))}
+          </div>
         </div>
-
-        {/* recent orders */}
-        <div className="bg-white px-2 p-4 mt-4 flex flex-col gap-2">
-          <h1 className="text-center text-l font-medium my-4 ">Recent Order</h1>
-          {recentOrders.map((items, index)=>
-                    <RecentOrder key={index} image={items.dishImg} dish={items.mealname} price={items.price} date={items.date} status={items.status}/>
-                  )}
-        </div>
-
-      </div>
-
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
